60fa3e0b563a92ff4809fd77a57dbd0e7b285963
Henry Chen <henrychen385@gmail.com>
2023-09-17 20:17:13 +0800

    reformatted sensor data into separate functions

--- app/src/tasks/i2cTask.c
+++ app/src/tasks/i2cTask.c
@@ -19,6 +19,44 @@ void I2CTask_init(struct I2CTask *task)
     }
 }
 
+int I2CTask_getImuData(struct I2CTask *task)
+{
+    int rc = sensor_sample_fetch(task->imu);
+
+    if (rc != 0)
+    {
+        printk("failed to fetch from IMU\n");
+    }
+
+    rc = sensor_channel_get(task->imu, SENSOR_CHAN_ACCEL_XYZ, &task->accel);
+    rc = sensor_channel_get(task->imu, SENSOR_CHAN_GYRO_XYZ, &task->gyro);
+
+    if (rc != 0)
+    {
+        printk("failed to get from IMU\n");
+    }
+
+    return rc;
+}
+
+int I2CTask_getBaroData(struct I2CTask *task)
+{
+    int rc = sensor_sample_fetch(task->baro);
+    if (rc != 0)
+    {
+        printk("failed to fetch from baro\n");
+    }
+    rc = sensor_channel_get(task->baro, SENSOR_CHAN_PRESS, &task->pressure);
+    rc = sensor_channel_get(task->baro, SENSOR_CHAN_AMBIENT_TEMP, &task->temperature);
+    rc = sensor_channel_get(task->baro, SENSOR_CHAN_HUMIDITY, &task->humidity);
+
+    if (rc != 0)
+    {
+        printk("failed to get get baro\n");
+    }
+    return rc;
+}
+
 // UserSpace Access
 void I2CTask_emit_imu_task(struct I2CTask *task)
 {
@@ -42,36 +80,25 @@ void I2CTask_thread(struct I2CTask *task, void *p2, void *p3)
 
         if ((0b1U == (0b1U & event)) != 0U)
         {
-            int rc = sensor_sample_fetch(task->imu);
-            if (rc != 0)
-            {
-                printk("failed to fetch from IMU\n");
-            }
-            rc = sensor_channel_get(task->imu, SENSOR_CHAN_ACCEL_XYZ, &task->accel);
-            rc = sensor_channel_get(task->imu, SENSOR_CHAN_GYRO_XYZ, &task->gyro);
+            int rc = I2CTask_getImuData(task);
 
             if (rc != 0)
             {
-                printk("failed to get from IMU\n");
+                printk("Failed to get IMU Data...\n");
             }
+
             k_event_clear(&appTask->events, 0b1U);
         }
 
         if ((0b10U == (0b10U & event)) != 0U)
         {
-            int rc = sensor_sample_fetch(task->baro);
-            if (rc != 0)
-            {
-                printk("failed to fetch from baro\n");
-            }
-            rc = sensor_channel_get(task->baro, SENSOR_CHAN_PRESS, &task->pressure);
-            rc = sensor_channel_get(task->baro, SENSOR_CHAN_AMBIENT_TEMP, &task->temperature);
-            rc = sensor_channel_get(task->baro, SENSOR_CHAN_HUMIDITY, &task->humidity);
+            int rc = I2CTask_getBaroData(task);
 
             if (rc != 0)
             {
-                printk("failed to get get baro\n");
+                printk("Failed to get Baro Data...\n");
             }
+
             k_event_clear(&appTask->events, 0b10U);
         }
 
